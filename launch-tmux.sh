bed76cf443e0bf76ce7b9aa38d657a1b7e08ed2c
Wouter Van de Weghe <wouter.van.de.weghe@sas.com>
2025-03-10 14:21:54 +0100

    Update launch-tmux.sh

--- launch-tmux.sh
+++ launch-tmux.sh
@@ -26,9 +26,9 @@ done
 NS=$namespace
 SessName=viya_watch_$NS
 tmux new -s $SessName -d
-tmux send-keys -t $SessName "watch -n 5 -d -c \"$HOME/bin/GetPodStatus.sh  -n ${NS}\" "  C-m
+tmux send-keys -t $SessName "watch -n 5 -d -c \"./GetPodStatus.sh  -n ${NS}\" "  C-m
 tmux split-window -v -p 70 -t $SessName
-tmux send-keys -t $SessName "watch -n 5   \"$HOME/bin/getuserpods.sh  -n ${NS}\" "  C-m
+tmux send-keys -t $SessName "watch -n 5   \"./getuserpods.sh  -n ${NS}\" "  C-m
 tmux split-window -v -p 70 -t $SessName
 tmux split-window -v -p 10 -t $SessName
 tmux send-keys -t $SessName "kubectl -n  ${NS} logs -f $(kubectl -n ${NS} get pods | grep read | awk '{ print $1}' ) | jq '.timeStamp, .message' "  C-m
